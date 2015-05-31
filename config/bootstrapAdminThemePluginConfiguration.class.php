caec30544ada21942df77d903718682a4160866c
brunopastor <brunopastor92@gmail.com>
2015-05-30 22:38:12 -0300

    config changes

--- config/bootstrapAdminThemePluginConfiguration.class.php
+++ config/bootstrapAdminThemePluginConfiguration.class.php
@@ -7,14 +7,7 @@ class bootstrapAdminThemePluginConfiguration extends sfPluginConfiguration
    */
   public function initialize()
   {
-    if (!sfConfig::get('sf_bootstrap_admin_module_web_dir'))
-    {
-      sfConfig::set('sf_bootstrap_admin_module_web_dir', '/bootstrapAdminThemePlugin');
-    }
-    
-    if (!sfConfig::get('sf_bootstrap_admin_module_responsive'))
-    {
-      sfConfig::set('sf_bootstrap_admin_module_responsive', false);
-    }
+      sfConfig::set('sf_bootstrap_admin_module_web_dir', '/bootstrapSymfonyThemeAdminPlugin/web');
+      sfConfig::set('sf_bootstrap_admin_module_responsive', true);
   }
 }
